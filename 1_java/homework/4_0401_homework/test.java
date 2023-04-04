b2c381771c908a6ed3019009fb9bdab8f5c41fed
Hycity <1006853851@qq.com>
2023-04-04 18:42:46 +0800

    修改了作业

--- 1_java/homework/4_0401_homework/test.java
+++ 1_java/homework/4_0401_homework/test.java
@@ -10,71 +10,109 @@ public class test {
         Manager lihua = new Manager("lihua",1234);
         Salesman wudong = new Salesman("wudong",3456);
         Worker xiaohua = new Worker("xiaohua",5555);
-
-        lihua.getSalary();
-        wudong.getSalary(10);
-        xiaohua.getSalary(24);
-
-        lihua.ToString();
-        wudong.ToString();
-        xiaohua.ToString();
+        
+        System.out.println(lihua.ToString());
+        System.out.println(wudong.ToString());
+        System.out.println(xiaohua.ToString());
     }
 }
 
 class Employee {
-    int id;
-    String name;
-    int salary;
-    Employee(){}
-    void getSalary() {
-        this.salary = 0;
+    protected int id;
+    protected String name;
+    final String post = "Employee";
+    final int salary = 0;
+
+    public Employee(){
+        this.id = 0;
+        this.name = "未知的名字";
     }
-    void getSalary(int cost) {
-        this.salary = cost;
+
+    public Employee(String name, int id) {
+        this.name = name;
+        this.id = id;
     }
-    void ToString() {
-        System.out.println(this.salary);
+
+    public int getSalary() {
+        return this.salary;
+    }
+
+    public String ToString() {
+        String str = String.format("职位:%s , 名字:%s, 工资:%d\n",this.post,this.name,this.salary);
+        return str;
     }
 }
 
 class Manager extends Employee{
-    Manager(String name, int id) {
-        this.name = name;
-        this.id = id;
+    final int salary = 5000; //固定工资
+    final String post = "Manager";
+
+    public Manager() {
+        super();
     }
-    void getSalary() {
-        super.getSalary(5000); //默认
+
+    public Manager(String name, int id) {
+        super(name,id);
     }
-    void getSalary(int cost) {
-        this.salary = cost; //可以输入参数来指定固定的工资
+
+    public int getSalary() {
+        return this.salary;
     }
-    void ToString() {
-        System.out.printf("Manager %s的工资是: %d\n",this.name,this.salary);
+    
+    public String ToString() {
+        String str = String.format("职位:%s , 名字:%s, 工资:%d\n",this.post,this.name,this.salary);
+        return str;
     }
 }
 
 class Salesman extends Employee{
-    Salesman(String name, int id) {
-        this.name = name;
-        this.id = id;
+    Scanner input = new Scanner(System.in);
+    final String post = "Salesman";
+    final int salary = 3000;
+
+    public Salesman() {
+        super();
     }
-    void getSalary(int n) {
-        this.salary = 3000+5*n; //根据销售量来进行提成
+
+    public Salesman(String name, int id) {
+        super(name,id);
     }
-    void ToString() {
-        System.out.printf("Salesman %s的工资是: %d\n",this.name,this.salary);
+    
+    public int getSalary() {
+        System.out.println("输入"+this.name+"的销售量: ");
+        int n = input.nextInt();
+        int salary = this.salary+5*n; //根据销售量来进行提成 这里假设在固定工资的基础上加5/件
+        return salary;
+    }
+    
+    public String ToString() {
+        String str = String.format("职位:%s , 名字:%s, 工资:%d\n",this.post,this.name,this.getSalary());
+        return str;
     }
 }
 
 class Worker extends Employee{
-    Worker(String name, int id) {
-        this.name = name;
-        this.id = id;
+    Scanner input = new Scanner(System.in);
+    final String post = "Worker";
+    final int salary = 200;
+
+    public Worker() {
+        super();
     }
-    void getSalary(int n) {
-        this.salary = 100*n; //根据工作天数来进行发工资
+
+    public Worker(String name, int id) {
+        super(name,id);
     }
-    void ToString() {
-        System.out.printf("Worker %s的工资是: %d\n",this.name,this.salary);
+    
+    public int getSalary() {
+        System.out.println("输入"+this.name+"的工作天数: ");
+        int n = input.nextInt();
+        int salary = this.salary * n; //根据工作天数来进行发工资
+        return salary;
+    }
+    
+    public String ToString() {
+        String str = String.format("职位:%s , 名字:%s, 工资:%d\n",this.post,this.name,this.getSalary());
+        return str;
     }
 }
--- 1_java/homework/4_0401_homework/test2.java
+++ 1_java/homework/4_0401_homework/test2.java
@@ -1,37 +1,32 @@
+import java.text.DecimalFormat;
+
 public class test2 {
     public static void main(String[] args) {
         Company meidi = new Company("meidi"); //美的公司
         Company geli = new Company("geli"); //格力公司
 
-        Person my = new Person("my",11,true);
-        Person you = new Person("my",11,true);
-        System.out.println(my.equals(you)); //判断两个人是不是同一个人
-
-        Employee lihua = new Employee("lihua",12,true,2000.0,meidi);
-        Employee hua = new Employee("lihua",12,true,20.0,meidi);
-        Employee yunlong = new Employee("yunlong",24,true,200.0,geli);
-        System.out.println(lihua.equals(hua)); //判断两个职员是不是同一个人
-        System.out.println(lihua.isSameCampany(hua)); //判断两人是不是一个公司的
-        System.out.println(lihua.isSameCampany(yunlong)); //判断两个是不是一个公司的
-
-        Student midi = new Student("midi",12,true,"No.123","class 2");
-        Student mi = new Student("midi",12,true,"No.123","class 2");
-        System.out.println(midi.equals(mi));//判断两个学生是不是同一个人
-
-        System.out.println("");
-        System.out.println(my.toString());
-        System.out.println(lihua.toString());
-        System.out.println(yunlong.toString());
-        System.out.println(meidi.toString());
-        System.out.println(midi.toString());
+        Employee em1 = new Employee("lihua",12,true,2000.4,meidi);
+        Employee em2 = new Employee("lihua",12,true,2000.4,meidi);
+        System.out.println(em1.equals(em2)); //判断两个职员是不是同一个人
+        System.out.println(em1.isSameCampany(em2)); //判断两个职员是不是同一个人
+        Employee em = new Employee();
+        System.out.println(em.company.name);
+        Student st1 = new Student("zhangsan",12,true,"No.123","class 2");
+        Student st2 = new Student("wanwu",12,true,"No.123","class 1");
+        st1.Hello(st2);
     }
 }
 
 class Person {
-    String name;
-    int age;
-    boolean gender; //性别,ture为男
+    protected String name;
+    protected int age;
+    protected boolean gender; //性别,true为男
 
+    public Person() {
+        this.name = "未知的名字";
+        this.age = 0;
+        this.gender = true;
+    }
     public Person(String name, int age, boolean gender) {
         this.name = name;
         this.age = age;
@@ -42,31 +37,24 @@ class Person {
         return str;
     }
     public boolean equals(Person obj) {
-        if (obj == null) {
+        if (obj == null)
             return false;
-        }
-        if (this.age == obj.age) {
-            if (this.name.equals(obj.name)) {
-                if (this.gender == obj.gender) {
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            }
-            else {
-                return false;
-            }
-        }
-        else {
+        if (this.age == obj.age && this.name.equals(obj.name) && this.gender == obj.gender)
+            return true;
+        else
             return false;
-        }
     }
 }
 
 class Student extends Person {
-    String stuNo;
-    String clazz;
+    protected String stuNo;
+    protected String clazz;
+
+    public Student() {
+        super();
+        this.stuNo = "未知的学号";
+        this.clazz = "未知的班级";
+    }
 
     public Student(String name, int age, boolean gender, String stuNo, String clazz) {
         super(name, age, gender);
@@ -81,31 +69,36 @@ class Student extends Person {
     }
 
     public boolean equals(Student obj) {
-        if (obj == null) {
+        if (obj == null)
             return false;
-        }
         boolean bbb = super.equals(obj);
-        if (bbb == true) {
-            if(this.stuNo.equals(obj.stuNo)) {
-                if (this.clazz.equals(obj.clazz)) {
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            }
-            else {
-                return false;
-            }
-        }
-        else {
+        if (bbb && this.stuNo.equals(obj.stuNo) && this.clazz.equals(obj.clazz))
+            return true;
+        else
             return false;
-        }
+    }
+
+    public boolean isSameClass(Student obj) {
+        if (this.clazz.equals(obj.clazz))
+            return true;
+        else
+            return false;
+    }
+
+    public void Hello(Student obj) {
+        if (this.isSameClass(obj))
+            System.out.println("hello, "+obj.name+", 我们的同班同学");
+        else
+            System.out.println("hello, "+obj.name);
     }
 }
 
 class Company {
-    String name;
+    protected String name;
+
+    public Company() {
+        this.name = "未知公司名";
+    }
 
     public Company(String name) {
         this.name = name;
@@ -116,21 +109,23 @@ class Company {
     }
 
     public boolean equals(Company obj) {
-        if (obj == null) {
+        if (obj == null)
             return false;
-        }
-        if (this.name.equals(obj.name)) {
+        if (this.name.equals(obj.name))
             return true;
-        }
-        else {
+        else
             return false;
-        }
     }
 }
 
 class Employee extends Person {
-    Company company;
-    double salary;
+    protected Company company = new Company(); //在类里面创建一个对象,这样子可以指定初值
+    protected double salary;
+
+    public Employee() {
+        super();
+        this.salary = 0;
+    }
 
     public Employee(String name, int age, boolean gender, double salary, Company company) {
         super(name, age, gender);
@@ -145,38 +140,31 @@ class Employee extends Person {
     }
 
     public boolean equals(Employee obj) {
-        if (obj == null) {
+        if (obj == null)
             return false;
-        }
         boolean bbb = super.equals(obj);
-        if (bbb == true) {
-            if (this.company.equals(obj.company)) {
-                if (this.salary == obj.salary) {
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            }
-            else {
-                return false;
-            }
-        }
-        else {
+        DecimalFormat myFormat = new DecimalFormat("#.#");
+        String str1 = myFormat.format(this.salary);
+        String str2 = myFormat.format(obj.salary);
+        if (bbb && this.company.equals(obj.company) && str1.equals(str2))
+            return true;
+        else
             return false;
-        }
     }
 
     public boolean isSameCampany(Employee obj) {
-        if (obj == null) {
+        if (obj == null)
             return false;
-        }
-        if (this.company.equals(obj.company)) {
+        if (this.company.equals(obj.company))
             return true;
-        }
-        else {
+        else
             return false;
-        }
     }
 }
 
+/** 
+ * 类的继承,多态,封装总结
+ * 继承, 子类可以继承父类的所有属性和方法
+ * 多态, 同一类中的方法可以进行重载, 同一继承树中的方法可以进行重写
+ * 封装, 通过访问限制修饰符还有非访问限制修饰符的使用,可以限定类中的属性和方法的访问权限,提高安全性
+ **/
